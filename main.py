78ace281b97d05083cd08652b35f3799c2ed4ba4
pandablue0809 <bluep0415@gmail.com>
2022-11-05 02:12:00 -0700

    bug fix: allow CORS else API deny access

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 '''fastapi App with its endpoints'''
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import nft, covalent
 
 
 app = FastAPI()
+
+
+def cors(app: FastAPI):
+    '''Enable CORS'''
+    try:
+        # the wildcard "*" says that all are allowed
+        app.add_middleware(
+                CORSMiddleware,
+                allow_origins=["*"],
+                allow_credentials=False,
+                allow_methods=["*"],
+                allow_headers=["*"],
+        )
+
+    except Exception as e:
+        print(f'\033[31m Error occurred while enabling CORS {e}\033[0m')
+
+
+cors(app)
 
 # base router
 @app.get('/')
