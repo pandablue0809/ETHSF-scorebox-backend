ed39ce1f9bb2b0506e463039d0434889f04b44b1
pandablue0809 <bluep0415@gmail.com>
2022-11-04 23:39:59 -0700

    #1 format NFT info fetched from OpenSea

--- routers/nft.py
+++ routers/nft.py
@@ -1,7 +1,7 @@
 '''OpenSea endpoint'''
 from fastapi import APIRouter, status
 from supp.schemas import OpenSeaNFT
-# from opensea import read_nfts
+from opensea import read_nfts
 
 
 router = APIRouter(
@@ -12,4 +12,31 @@ router = APIRouter(
 
 @router.post('/getnft', status_code=status.HTTP_200_OK)
 async def get_nft_assets(item: OpenSeaNFT):
-    return 'Forgot NFT router. Try now. Should work'
+    '''
+    Get NFT info
+
+    Input:
+    - **address [str]**: MetaMask wallet address
+
+    Output:
+    - **[object]**: NFT info
+    '''
+    req = read_nfts(item.address)
+    assets = req['assets']
+
+    res = dict(
+        total=0,
+        nft_count=0,
+        nft_with_value=0
+    )
+    if not assets:
+        print('ERR: No assets found')
+        return res
+
+    res['nft_count'] = len(assets)
+    for asset in assets:
+        if asset['last_sale']:
+            res['total'] += float(asset['last_sale']['payment_token']['usd_price'])
+            res['nft_with_value'] += 1
+            
+    return res
