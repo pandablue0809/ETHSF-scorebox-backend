753362567d4d5787c3ffa49bccdafe13c023400e
pandablue0809 <bluep0415@gmail.com>
2022-11-05 01:08:18 -0700

    #2 hit Covalent HQ 3 endpoints

--- routers/covalent.py
+++ routers/covalent.py
@@ -12,4 +12,12 @@ router = APIRouter(
 @router.post('/covalent', status_code=status.HTTP_200_OK)
 async def credit_score_covalent(item: Covalent):
     '''Calculates credit score'''
+    txn = covalent_get_txn(
+            str(item.chain_id),
+            item.eth_address,
+            item.covalent_key,
+            False,
+            500, 
+            0
+            )
     return 'test this endpoint on Swagger'
